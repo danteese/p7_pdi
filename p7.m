4ef5375d3a222b1e4cd37bca9ca3ca4c66107add
danteese <dalnte@me.com>
2019-04-25 12:52:34 -0500

    Parte 4

--- p7.m
+++ p7.m
@@ -1,3 +1,16 @@
 % Integrantes: 
 % Dante Bazaldua 
 % Leonardo Lopez
+
+%% Parte 2
+clc; clear;
+img = imread('flexible.tif');
+
+gray = mat2gray(double(img));
+% Fourier transform
+img_fft = fft2(gray);
+img_fftsh = fftshift(img_fft);
+
+[ix, iy] = size(img_fftsh);
+vx = -1:1/230:1;
+vy = -1:1/246:1;
